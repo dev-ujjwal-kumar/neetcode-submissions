a8fcaf805c79b4c716ea90f1da56e94ee4aacca9
Ujjwal Kumar <42748562+dev-ujjwal-kumar@users.noreply.github.com>
2026-08-29 12:20:02 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    private void subsetDfs(int[] nums, List<Integer> subset, List<List<Integer>> res, int level){
+        if(level == nums.length){
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+        // inclusion call
+        subset.add(nums[level]);
+        subsetDfs(nums, subset, res, level+1);
+        subset.remove(subset.size() - 1); // Backtracking
+        subsetDfs(nums, subset, res, level+1); // Exclusion call
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        subsetDfs(nums, subset, res, 0);
+        return res;
+    }
+}
